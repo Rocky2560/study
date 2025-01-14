c7367f786eb762f5cdc2fdf540d5a577c2b41ae3
rocky2560 <sudipmanandhar2560@gmail.com>
2025-01-14 13:12:22 +0000

    binary tree preoder and in order impelemented

--- src/main/java/org/example/DSA/binaryTree.java
+++ src/main/java/org/example/DSA/binaryTree.java
@@ -46,7 +46,87 @@ public class binaryTree {
             preOrder(root.right);
         }
 
-        //Stack implementation to print the data
+        public void inOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return ;
+            }
+            inOrder(root.left);
+            System.out.println(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public void postOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.println(root.data + " ");
+        }
+
+        //Stack implementation to print the sata postorder
+    public void preOrderStack(TreeNode root)
+    {
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (current != null || !stack.isEmpty())
+        {
+            if (current != null)
+            {
+                stack.push(current);
+                current = current.left;
+            }
+            else
+            {
+                TreeNode temp = stack.peek().right;
+                if (temp == null)
+                {
+                    temp = stack.pop();
+                    System.out.println(temp.data + " ");
+                    while (!stack.isEmpty() && temp == stack.peek().right)
+                    {
+                        temp = stack.pop();
+                        System.out.println(temp.data + " ");
+                    }
+                }
+                else
+                {
+                    current = temp;
+                }
+            }
+        }
+    }
+        //Stack impleemntation to print the data inorder
+    public void inOrderStack()
+    {
+    if ( root == null)
+    {
+        return;
+    }
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode temp = root;
+    while (!stack.isEmpty() || temp != null)
+    {
+        if (temp != null)
+        {
+            stack.push(temp);
+            temp = temp.left;
+        }
+        else
+        {
+            temp = stack.pop();
+            System.out.print(temp.data + " ");
+            temp = temp.right;
+        }
+    }
+
+    }
+
+        //Stack implementation to print the data preorder
         public void preOrderStackImpl()
         {
             if (root == null)
@@ -75,8 +155,11 @@ public class binaryTree {
 
             binaryTree binaryTree = new binaryTree();
             binaryTree.createBinaryTree();
-            binaryTree.preOrder(binaryTree.root);
-            binaryTree.preOrderStackImpl();
+//            binaryTree.preOrder(binaryTree.root);
+//            binaryTree.preOrderStackImpl();
+//            binaryTree.postOrder(binaryTree.root);
+//            binaryTree.inOrderStack();
+            binaryTree.preOrderStack(binaryTree.root);
 
 
         }
