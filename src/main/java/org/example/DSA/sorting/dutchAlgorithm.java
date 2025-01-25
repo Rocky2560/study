42dce623e8d02e88a375761739d32ec05b4c2d4a
rocky2560 <sudipmanandhar2560@gmail.com>
2025-01-25 12:02:50 +0000

    ducth natinal flag is in process of learning

---
+++ src/main/java/org/example/DSA/sorting/dutchAlgorithm.java
@@ -0,0 +1,31 @@
+package org.example.DSA.sorting;
+
+public class dutchAlgorithm {
+
+    public static void ducthNational(int[] arr) {
+        int i = 0, j = 0, k = arr.length - 1;
+        while (i <= k) {
+            if (arr[i] == 0) {
+                swap(arr, i, j);
+                i++;
+                j++;
+            } else if (arr[i] == 1) {
+                i++;
+            } else if (arr[i] == 2) {
+                swap(arr, i, k);
+                k--;
+            }
+        }
+    }
+        public static void swap (int [] arr, int i, int k)
+        {
+            int temp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = temp;
+        }
+
+    public static void main(String[] args) {
+        int arr [] = {0,1,0,0,2,1,2,1};
+        ducthNational(arr);
+    }
+}
