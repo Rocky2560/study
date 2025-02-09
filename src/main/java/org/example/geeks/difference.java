7c6afe33db21be59b3941e8f801f61f10702c9c6
rocky2560 <sudipmanandhar2560@gmail.com>
2025-02-09 22:08:34 +0000

    the minumum difference solution is optimised|

--- src/main/java/org/example/geeks/difference.java
+++ src/main/java/org/example/geeks/difference.java
@@ -23,6 +23,24 @@ public class difference {
 
     }
 
+    public void optimizedSolution(int [] a, int diff)
+    {
+        int i=0, j=1;
+        int n = a.length;
+        while (i < n && j < n)
+        {
+            if ( i != j && (a[j] - a[i] == diff || a[i] - a[j] == diff))
+            {
+                System.out.println("Pair found"+ a[i] + a[j]);
+            }
+            else if (a[j] - a[i] < n)
+                j++;
+            else
+                i++;
+        }
+        System.out.println("no such pair");
+    }
+
     public static void main(String[] args) {
         // Input array and diff
         int[] arr = { 1, 8, 30, 40, 100 };
