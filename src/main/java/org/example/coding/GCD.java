3ad737aeba33bb2051819e44643ed59466761004
rocky2560 <sudipmanandhar2560@gmail.com>
2025-04-26 14:56:11 +0100

    GCD problem solved

--- src/main/java/org/example/coding/GCD.java
+++ src/main/java/org/example/coding/GCD.java
@@ -1,4 +1,13 @@
 package org.example.coding;
 
 public class GCD {
+    public static int gcd(int a, int b) {
+        // code here
+        return (b == 0) ? a:gcd(b, a%b);
+    }
+    public static void main(String[] args) {
+        int a =3, b=6;
+        int op = gcd(a,b);
+        System.out.println(op);
+    }
 }
