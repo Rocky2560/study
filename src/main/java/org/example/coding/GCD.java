37e04f6309ede77b19b79aaed23acd5e2e39aecb
rocky2560 <sudipmanandhar2560@gmail.com>
2025-05-09 10:58:28 +0100

    GCD of array solved

--- src/main/java/org/example/coding/GCD.java
+++ src/main/java/org/example/coding/GCD.java
@@ -5,9 +5,19 @@ public class GCD {
         // code here
         return (b == 0) ? a:gcd(b, a%b);
     }
+
+
     public static void main(String[] args) {
         int a =3, b=6;
         int op = gcd(a,b);
         System.out.println(op);
+
+        int [] x = {2,4,6,8};
+        int gcd = x[0];
+        for (int i=1; i<x.length; i++)
+        {
+            gcd =  gcd(gcd,x[i]);
+        }
+        System.out.println(gcd);
     }
 }
