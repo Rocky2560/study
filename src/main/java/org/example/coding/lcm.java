eeb56574ff479e5a757c7a5ef2be83a868e4891e
rocky2560 <sudipmanandhar2560@gmail.com>
2025-05-09 04:44:59 +0100

    Euclidean algorithm solved

--- src/main/java/org/example/coding/lcm.java
+++ src/main/java/org/example/coding/lcm.java
@@ -1,4 +1,27 @@
 package org.example.coding;
 
 public class lcm {
+
+    public static int gcd(int a, int b)
+    {
+        while (b !=0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm (int a, int b)
+    {
+        int n = (Math.abs(a * b)) / gcd(a, b);
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int b=9, a=3;
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a, b));
+    }
 }
