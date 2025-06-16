efbdd990b5e316ebd994138cda1b29d081a8bb8b
rocky2560 <sudipmanandhar2560@gmail.com>
2025-06-16 17:55:39 +0100

    sum of array has been calculated

---
+++ src/main/java/org/example/coding/sumofArray.java
@@ -0,0 +1,13 @@
+package org.example.coding;
+
+public class sumofArray {
+    public static void main(String[] args) {
+        int a [] = {2,3};
+        int sum =0;
+        for(int x: a)
+        {
+            sum+=x;
+        }
+        System.out.println(sum);
+    }
+}
