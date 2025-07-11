edbbd1474438218d2ceded138fe069804dd3d479
rocky2560 <sudipmanandhar2560@gmail.com>
2025-07-11 10:31:15 +0100

    String reversed problem solved

--- src/main/java/org/example/interview/reverseString.java
+++ src/main/java/org/example/interview/reverseString.java
@@ -3,8 +3,8 @@ package org.example.interview;
 public class reverseString {
     public static void main(String[] args) {
         String s = "sudip";
-        StringBuilder stringBuilder = new StringBuilder(s);
-        stringBuilder.reverse();
+//        StringBuilder stringBuilder = new StringBuilder(s);
+//        stringBuilder.reverse();
         for (int i=s.length()-1 ; i >= 0; i--)
         {
             stringBuilder.append(s.charAt(i));
