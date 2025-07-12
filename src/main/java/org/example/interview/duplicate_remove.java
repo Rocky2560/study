893f330275a2f83142b625c176e906264f0ae386
rocky2560 <sudipmanandhar2560@gmail.com>
2025-07-12 09:33:29 +0100

    duplicates in unsorted array removed problem solved

---
+++ src/main/java/org/example/interview/duplicate_remove.java
@@ -0,0 +1,20 @@
+package org.example.interview;
+import java.util.*;
+public class duplicate_remove {
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,2,1};
+        HashSet<Integer> op = new HashSet<Integer>();
+        for (int x: arr)
+        {
+            op.add(x);
+        }
+        int [] a = new int[op.size()];
+        int i=0;
+        for (int y: op)
+        {
+            a[i++]=y;
+        }
+        System.out.println("Arrays after removing duplciates"+Arrays.toString(a));
+    }
+}
