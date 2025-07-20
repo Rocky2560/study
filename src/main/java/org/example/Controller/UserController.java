2054f04499b00d8bf61a2b61414a9affa0496e6e
rocky2560 <sudipmanandhar2560@gmail.com>
2025-07-20 18:21:09 +0100

    microservice architecture added

---
+++ src/main/java/org/example/Controller/UserController.java
@@ -0,0 +1,17 @@
+package org.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public String getUSerById(@PathVariable String id)
+    {
+        return "User" + id + "from USer Service";
+    }
+}
