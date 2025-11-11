cc6d725d25a464c10dbb1f9326c02491c374063b
rocky2560 <sudipmanandhar2560@gmail.com>
2025-11-11 18:06:30 +0545

    multiplication table problem solved

---
+++ src/main/java/org/example/Nov11/Multiplication_Table.java
@@ -0,0 +1,16 @@
+package org.example.Nov11;
+
+import java.util.Arrays;
+
+public class Multiplication_Table {
+
+    public static void main(String[] args) {
+        int [] op = new int[100];
+        int n= 2;
+        for (int i = 1; i <= 10; i++) {
+            op[i-1] = i*n;
+            System.out.println(op[i-1]);
+        }
+
+    }
+}
