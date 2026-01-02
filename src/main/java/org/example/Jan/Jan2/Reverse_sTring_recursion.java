9b8d8507c88acf5a4ee67dd62355081283829c87
rocky2560 <sudipmanandhar2560@gmail.com>
2026-01-02 09:01:31 +0545

    recusrion implememted in string reverse

---
+++ src/main/java/org/example/Jan/Jan2/Reverse_sTring_recursion.java
@@ -0,0 +1,26 @@
+package org.example.Jan.Jan2;
+//Given a string, the task is to print the given string in reverse order using recursion.
+//
+//Examples:
+//
+//Input: s = "Geeks for Geeks"
+//Output: "skeeG rof skeeG"
+//Explanation: After reversing the input string we get "skeeG rof skeeG".
+//
+//Input: s = "Reverse a string Using Recursion"
+//Output: "noisruceR gnisU gnirts a esreveR"
+//Explanation: After reversing the input string we get "noisruceR gnisU gnirts a esreveR".
+
+public class Reverse_sTring_recursion {
+
+
+    public static void main(String[] args) {
+        String str = "Geeks for pidus";
+        System.out.println(reverse(str));
+    }
+
+    public static String reverse(String str) {
+    if (str.isEmpty()) return str;
+    return reverse(str.substring(1))+ str.charAt(0);
+    }
+}
